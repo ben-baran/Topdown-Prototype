47a7ce3e5f67e65730eb40140ef06bfa193bb4dd
Ben Barantschik <bbarantschik@gmail.com>
2015-07-09 21:59:03 -0700

    fixed positioning of weapon

--- src/com/barantschik/game/run/topdown/Weapon.java
+++ src/com/barantschik/game/run/topdown/Weapon.java
@@ -19,6 +19,7 @@ public class Weapon extends Box
 		setupBox();
 		
 		setLayer(-3);
+		setImportance(3);
 		setShader(GUtil.getWhiteShader());
 	}
 	
@@ -26,9 +27,12 @@ public class Weapon extends Box
 	{
 		Box player = (Box) Scene.get("player");
 		
-		getCenterX().set(player.getCenterX().get() + );
-		getCenterY().set(player.getCenterY().get());
-		getTheta().set(player.getTheta().get());
+		double sin = Math.sin(player.getTheta().get()), cos = Math.cos(player.getTheta().get());
+		double[] v1 = {cos, -sin}, v2 = {sin, cos};
+		
+		getCenterX().set(player.getCenterX().get() + 50 * v1[0] + 70 * v2[0]);
+		getCenterY().set(player.getCenterY().get() + 50 * v1[1] + 70 * v2[1]);
+		getTheta().set(player.getTheta().get() - 0.8);
 		super.update();
 	}
 }
